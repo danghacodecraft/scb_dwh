9d2b73f3972001b0160b8a7e0ee6c9ca47258585
caoxuanbaoloc <loc@minerva.vn>
2021-11-29 14:36:47 +0700

    update chart loan

--- api/v1/report/business_unit/capital/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from api.v1.report.business_unit.capital.views import CapitalBusinessView
-
-urlpatterns = [
-    path('data/', CapitalBusinessView.as_view({'get': 'data'})),
-    path('chart/', CapitalBusinessView.as_view({'post': 'chart'}))
-]
