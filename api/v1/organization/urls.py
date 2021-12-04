b14d9f29da0e6024a52905854033d8cddd874efb
caoxuanbaoloc <loc@minerva.vn>
2021-12-04 11:12:12 +0700

    update chart list

--- api/v1/organization/urls.py
+++ api/v1/organization/urls.py
@@ -4,6 +4,8 @@ from api.v1.organization.views import OrganizationView
 
 urlpatterns = [
     path('data/', OrganizationView.as_view({'get': 'data'})),
+    path('list/', OrganizationView.as_view({'get': 'list'})),
+
     path('region/', OrganizationView.as_view({'get': 'region'})),
     path('branch/', OrganizationView.as_view({'get': 'branch'}))
 
