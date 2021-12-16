75f5d141795d6ade21b057b6e26fd02eb5d389af
caoxuanbaoloc <loc@minerva.vn>
2021-12-16 16:48:34 +0700

    update search

--- api/v1/gis/serializers.py
+++ api/v1/gis/serializers.py
@@ -7,6 +7,8 @@ from config.settings import DATETIME_INPUT_OUTPUT_FORMAT
 #     region = serializers.CharField(help_text="`region` of branch")
 
 class BranchResponseSerializer(InheritedSerializer):
+    region_id = serializers.IntegerField(help_text="`region_id` of branch")
+    region_name = serializers.IntegerField(help_text="`region_name` of branch")
     branch_id = serializers.IntegerField(help_text="`id` of branch")
     branch_name = serializers.CharField(help_text="`name` of branch")
     latitude = serializers.FloatField(help_text="`latitude` of branch")
--- api/v1/gis/urls.py
+++ api/v1/gis/urls.py
@@ -4,6 +4,7 @@ from api.v1.gis.views import GisView
 
 urlpatterns = [
     path('region/', GisView.as_view({'get': 'region'})),
-    path('branch/', GisView.as_view({'get': 'branch'}))
+    path('branch/', GisView.as_view({'get': 'branch'})),
+    path('search/', GisView.as_view({'get': 'search'}))
 
 ]
