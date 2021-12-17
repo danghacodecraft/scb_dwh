59572239a128eb7638d01c3e37b3b0a27804986d
caoxuanbaoloc <loc@minerva.vn>
2021-12-17 11:39:59 +0700

    update filter vung, dv

--- api/v1/employee/serializers.py
+++ api/v1/employee/serializers.py
@@ -3,21 +3,20 @@ from rest_framework import serializers
 from api.base.serializers import InheritedSerializer
 from config.settings import DATETIME_INPUT_OUTPUT_FORMAT
 
-# class ChartRequestSerializer(InheritedSerializer):
-#     module = serializers.CharField(help_text="`module` of chart")
+class EmployeeResponseSerializer(InheritedSerializer):
+    emp_id = serializers.CharField(help_text="`emp_id` of data")
+    emp_name = serializers.CharField(help_text="`emp_name` of data")
+    title = serializers.CharField(help_text="`title` of data")
+    dep_id = serializers.CharField(help_text="`dep_id` of data")
+    dep_name = serializers.CharField(help_text="`dep_name` of data")
+    time = serializers.CharField(help_text="`time` of data")
+    email = serializers.CharField(help_text="`email` of data")
+    mobile = serializers.CharField(help_text="`mobile` of data")
+    avatar = serializers.CharField(help_text="`avatar` of data")
 
-class DataResponseSerializer(InheritedSerializer):
-    id = serializers.CharField(help_text="`id` of data")
-    parent_id = serializers.CharField(help_text="`parent_id` of data")
-    fullname = serializers.CharField(help_text="`fullname` of data")
-    level = serializers.IntegerField(help_text="`level` of data")
 
 class BranchResponseSerializer(InheritedSerializer):
     id = serializers.CharField(help_text="`id` of data")
     fullname = serializers.CharField(help_text="`fullname` of data")
     level = serializers.CharField(help_text="`level` of data")
 
-
-class RegionResponseSerializer(InheritedSerializer):
-    region_id = serializers.IntegerField(help_text="`id` of region")
-    region_name = serializers.CharField(help_text="`name` of region")
--- api/v1/employee/urls.py
+++ api/v1/employee/urls.py
@@ -1,20 +1,10 @@
 from django.urls import path
 
-from api.v1.organization.views import OrganizationView
+from api.v1.employee.views import EmployeeView
 
 urlpatterns = [
-    path('data/', OrganizationView.as_view({'get': 'data'})),
-
-    path('region/', OrganizationView.as_view({'get': 'region'})),
-    path('region_list/', OrganizationView.as_view({'get': 'region_list'})),
-
-    path('branch/', OrganizationView.as_view({'get': 'branch'})),
-    path('branch_list/', OrganizationView.as_view({'get': 'branch_list'})),
-
-
-    # path('home/', OrganizationView.as_view({'get': 'home'})),
-    path('emp_list/', OrganizationView.as_view({'get': 'emp_list'})),
-    path('emp_detail/', OrganizationView.as_view({'get': 'emp_detail'})),
-    path('dep_list/', OrganizationView.as_view({'get': 'dep_list'})),
+    path('emp_list/', EmployeeView.as_view({'get': 'emp_list'})),
+    path('emp_detail/', EmployeeView.as_view({'get': 'emp_detail'})),
+    path('dep_list/', EmployeeView.as_view({'get': 'dep_list'})),
 
 ]
