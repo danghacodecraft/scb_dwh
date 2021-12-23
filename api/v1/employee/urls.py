9f49b23dfd88138d56f032fb9349bbde396e0b76
caoxuanbaoloc <loc@minerva.vn>
2021-12-23 17:16:20 +0700

    /api/v1/employee/emp_detail_kpi/?emp=14856

--- api/v1/employee/urls.py
+++ api/v1/employee/urls.py
@@ -5,7 +5,6 @@ from api.v1.employee.views import EmployeeView
 urlpatterns = [
     path('emp_list/', EmployeeView.as_view({'get': 'emp_list'})),
     path('emp_detail/', EmployeeView.as_view({'get': 'emp_detail'})),
+    path('emp_detail_kpi/', EmployeeView.as_view({'get': 'emp_detail_kpi'})),
     path('dep_list/', EmployeeView.as_view({'get': 'dep_list'})),
-
-    path('brn_detail/', EmployeeView.as_view({'get': 'brn_detail'})),
 ]
