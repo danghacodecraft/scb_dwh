4ec66750980266870dfacf4ab4c9d93bac611541
TrungMy <trung@minerva.vn>
2022-04-28 15:43:52 +0700

    Up lại

--- api/v1/views.py
+++ api/v1/views.py
@@ -24,7 +24,7 @@ def get_health(request):
 
         response = Response()
 
-        response.headers['SRV-Time'] = now()
+        response.headers['SRV-Time'] = now().isoformat(timespec='microseconds')
 
         response.status_code = status.HTTP_200_OK
 
