cf40c399acf1081b1ac0536b9364fab63a4e6f63
TrungMy <trung@minerva.vn>
2022-04-28 15:35:07 +0700

    Bổ sung check-health

--- api/v1/views.py
+++ api/v1/views.py
@@ -5,13 +5,27 @@ from rest_framework.decorators import api_view
 
 
 from library.otp import get_otp
+from library.functions import now
 
 
 @api_view(['GET'])
 def get_otp_server(request):
     if request.method == 'GET':
-        _otp = get_otp(10)
+        _otp = get_otp(30)
 
         status_code = status.HTTP_200_OK
 
         return Response({'otp': _otp[1]}, status=status_code)
+
+
+@ api_view(['GET'])
+def get_health(request):
+    if request.method == 'GET':
+
+        response = Response()
+
+        response.headers['SRV-Time'] = now()
+
+        response.status_code = status.HTTP_200_OK
+
+        return response
