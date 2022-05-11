8b626ad612b277d4fd0f6ea074e1ded9bcea5719
Phạm Thành Đạt <datpt@minerva.vn>
2022-05-11 15:42:14 +0700

    [fix] sv dashboard

--- api/sv/dashboard/urls.py
+++ api/sv/dashboard/urls.py
@@ -5,7 +5,7 @@ from api.sv.dashboard.views import DashboardSVView
 
 urlpatterns = [
     path('data/', DashboardSVView.as_view({'get': 'data'})),
-    path('dataex/', DashboardView.as_view({'get': 'dataex'})),
-    path('chart/', DashboardView.as_view({'get': 'chart'})),
+    path('dataex/', DashboardSVView.as_view({'get': 'dataex'})),
+    path('chart/', DashboardSVView.as_view({'get': 'chart'})),
 
 ]
