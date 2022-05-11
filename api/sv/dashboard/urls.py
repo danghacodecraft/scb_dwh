2ba8a68767d167b2fbf20b2cf180344f9aa31bea
Phạm Thành Đạt <datpt@minerva.vn>
2022-05-11 09:59:48 +0700

    [add] sv dashboard

---
+++ api/sv/dashboard/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from api.sv.dashboard.views import DashboardSVView
+
+
+urlpatterns = [
+    path('data/', DashboardSVView.as_view({'get': 'data'})),
+    # path('dataex/', DashboardView.as_view({'get': 'dataex'})),
+    # path('chart/', DashboardView.as_view({'get': 'chart'})),
+
+]
--- api/sv/urls.py
+++ api/sv/urls.py
@@ -3,4 +3,5 @@ from django.urls import include, path
 urlpatterns = [
     path('v1/employee/', include('api.sv.employee.urls')),
     path('v1/gis/', include('api.sv.gis.urls')),
+    path('v1/dashboard/', include('api.sv.dashboard.urls')),
 ]
